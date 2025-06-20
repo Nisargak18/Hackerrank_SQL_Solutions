0ffc8d3a49b2792a2004ee360b2a689cca7e8f06
Nisarga Kenchangoudr <143694059+Nisargak18@users.noreply.github.com>
2025-06-20 12:57:55 +0530

    Add files via upload

---
+++ Revising the select query 2.sql
@@ -0,0 +1,10 @@
+Query the NAME field for all American cities in the CITY table with populations larger than 120000. The CountryCode for America is USA.
+
+The CITY table is described as follows:
+CITY.jpg
+
+
+Solution:
+
+SELECT name FROM city
+WHERE  countrycode='USA' AND population > 120000;
